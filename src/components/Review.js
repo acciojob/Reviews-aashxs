02e67bed12ec0dff38f903105afdbdff5e2a3379
aashxs <aashxsh@gmail.com>
2024-09-25 09:46:22 +0530

    Commit -1

---
+++ src/components/Review.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Review = (props) => {
+  return (
+    <div>
+      <p className='author' id={`author-id${props.id}`}>{props.name}</p>
+      <p className='job'>{props.job}</p>
+      <img style={{
+        width:"100px"
+      }} className='person-img' src={props.image} alt=""/>
+      <p className='info'>{props.text}</p>
+    </div>
+  )
+}
+
+export default Review
