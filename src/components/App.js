48c553e67085de7c3dc124f54a443ca4c02b8c0f
aashxs <aashxsh@gmail.com>
2024-09-25 10:13:13 +0530

    Commit -4

--- src/components/App.js
+++ src/components/App.js
@@ -1,96 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Review from './Review';
 
 const App = () => {
-  const [current, Next] = useState(1);
-
-  const previous = () => {
-    if (current > 1) {
-      Next(current - 1);
-    }
-  };
-
-  const next = () => {
-    if (current < 4) {
-      Next(current + 1);
-    }
-  };
-
-  const random = () => {
-    let num = current;
-    while (num === current) {
-      num = Math.floor(Math.random() * 4) + 1;
-    }
-    Next(num);
-  };
-
-  const reviews = [
-    {
-      id: 1,
-      name: 'susan smith',
-      job: 'web developer',
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-      text:
-        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-    },
-    {
-      id: 2,
-      name: 'anna johnson',
-      job: 'web designer',
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
-      text:
-        'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
-    },
-    {
-      id: 3,
-      name: 'peter jones',
-      job: 'intern',
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-      text:
-        'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
-    },
-    {
-      id: 4,
-      name: 'bill anderson',
-      job: 'the boss',
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
-      text:
-        'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.',
-    },
-  ];
-
-  return (
-    <div>
-      <h1 id="review-heading">Our Reviews</h1>
-      {reviews.map((data, index) =>
-        data.id === current ? (
-          <Review
-            key={index}
-            name={data.name}
-            id={data.id}
-            image={data.image}
-            text={data.text}
-            job={data.job}
-          />
-        ) : null
-      )}
-
-      <button onClick={previous} className="prev-btn">
-        Previous
-      </button>
-      <button onClick={random} className="random-btn">
-        Surprise me
-      </button>
-      <button onClick={next} className="next-btn">
-        Next
-      </button>
-    </div>
-  );
+    return (
+        <main>
+            <section className="container">
+                <h1 id="review-heading">Our Reviews</h1>
+                <Review />
+            </section>
+        </main>
+    );
 };
 
 export default App;
-
--- src/components/Review.js
+++ src/components/Review.js
@@ -1,18 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react';
 
+// Predefined reviews data
+const reviews = [
+    {
+        id: 1,
+        name: 'susan smith',
+        job: 'web developer',
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+        text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up.",
+    },
+    {
+        id: 2,
+        name: 'anna johnson',
+        job: 'web designer',
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+        text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle.',
+    },
+    {
+        id: 3,
+        name: 'peter jones',
+        job: 'intern',
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+        text: 'Sriracha literally flexitarian irony, vape marfa unicorn.',
+    },
+    {
+        id: 4,
+        name: 'bill anderson',
+        job: 'the boss',
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+        text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo.',
+    }
+];
 
-const Review = ({ name, id, image, text, job }) => {
+const Review = () => {
+    const [index, setIndex] = useState(0); // State to manage the current review index
+    const { id, name, job, image, text } = reviews[index]; // Destructure current review
+
+    // Function to go to the previous review
+    const prevReview = () => {
+        setIndex(index === 0 ? reviews.length - 1 : index - 1);
+    };
+
+    // Function to go to the next review
+    const nextReview = () => {
+        setIndex(index === reviews.length - 1 ? 0 : index + 1);
+    };
+
+    // Function to display a random review, avoiding the current index
+    const randomReview = () => {
+        let randomIndex;
+        do {
+            randomIndex = Math.floor(Math.random() * reviews.length);
+        } while (randomIndex === index);
+        setIndex(randomIndex);
+    };
+
     return (
-      <article className="review">
-        <div>
-          <img src={image} alt={name} />
-          <h4 id={`author-${id}`}>{name}</h4>
-          <p>{job}</p>
-          <p>{text}</p>
-        </div>
-      </article>
+        <article className="review">
+            <img src={image} alt={name} className="person-img" />
+            <h4 id={`author-${id}`} className="author">{name}</h4>
+            <p className="job">{job}</p>
+            <p className="info">{text}</p>
+            <div className="button-container">
+                <button className="prev-btn" onClick={prevReview}>Prev</button>
+                <button className="next-btn" onClick={nextReview}>Next</button>
+                <button className="random-btn" onClick={randomReview}>Surprise Me</button>
+            </div>
+        </article>
     );
-  };
-  
-  export default Review;
-  
+};
+
+export default Review;
